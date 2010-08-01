c91429a2733d1f7ee848031119267493a6c6045a
sorcerersr <sorcerersr@0a7c3cb3-43ac-0efc-a4cb-ac9a25e90ac0>
2010-08-01 11:18:46 +0000

    A dialog for entering the password for a passworded game server
    git-svn-id: http://urtsb.googlecode.com/svn/trunk@14 0a7c3cb3-43ac-0efc-a4cb-ac9a25e90ac0

---
+++ src/passworddialog.py
@@ -0,0 +1,87 @@
+#
+# Copyright (C) 2010  Sorcerer
+#
+# This file is part of UrTSB.
+#
+# UrTSB is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# UrTSB is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with UrTSB.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from guicontroller import GuiController
+import gtk
+
+class PasswordDialog(gtk.Dialog):
+    """
+    A dialog for entering the password for a passworded game server
+    """
+
+
+    def __init__(self, server):
+        """
+        Constructor
+        
+        @param server - the server which needs a password
+        """
+        gtk.Dialog.__init__(self, 'Server needs a password to connect!', None,\
+                              gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT)
+        
+        self.server = server
+        #buttons
+        okbutton = gtk.Button('OK')
+        cancelbutton = gtk.Button('Cancel')
+        
+        okbutton.connect("clicked", self.on_ok_clicked)
+        cancelbutton.connect("clicked", self.on_cancel_clicked)
+        
+        
+        self.action_area.pack_start(cancelbutton, False, False)
+        self.action_area.pack_start(okbutton, False, False)
+        
+        #content area
+        desc_label = gtk.Label('The server you are trying to connect needs a password')
+        namelabel = gtk.Label('Servername: ' + server.getName())
+        adresslabel = gtk.Label('Serveradress: ' + server.getAdress())
+        self.passentry = gtk.Entry()
+        self.passentry.set_visibility(False)
+        self.passentry.set_text(server.getPassword())
+        
+        self.vbox.pack_start(desc_label, False, False)
+        self.vbox.pack_start(namelabel, False, False)
+        self.vbox.pack_start(adresslabel, False, False)
+        self.vbox.pack_start(self.passentry, False, False)
+        
+        self.show_all()
+        
+        
+    def on_ok_clicked(self, widget):
+            """
+            Callback of the OK button
+            """
+            #get the entered password
+            password = self.passentry.get_text()
+            #set the password at the server object
+            self.server.setPassword(password)
+            #and connect...
+            gui = GuiController()
+            gui.connectToServer(self.server)
+            #afterwards close the dialog
+            self.destroy()
+            
+    def on_cancel_clicked(self, widger):
+            """
+            Callback of the Cancel button
+            """
+            #do nothing just close the dialog
+            self.destroy()
+        
+        
