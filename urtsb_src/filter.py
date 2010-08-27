71f818cf73956a40286e567f2956bb707af5987e
sorcerersr <sorcerersr@0a7c3cb3-43ac-0efc-a4cb-ac9a25e90ac0>
2010-08-27 17:29:41 +0000

    fixed displayed server count
    git-svn-id: http://urtsb.googlecode.com/svn/trunk@168 0a7c3cb3-43ac-0efc-a4cb-ac9a25e90ac0

--- urtsb_src/filter.py
+++ urtsb_src/filter.py
@@ -301,6 +301,8 @@ class Filter(object):
         @returns True if a buddy is playing on the server
         """
        
+        Log.log.debug('[Filter] checking server' + server.to_string() \
+                      + ' for playing buddies')
         playerlist = server.getPlayerList()
         
         #if there are no players on the server return true to hide the server
